0bcf830d9ab506640cc00926a0b3b1d5575363a5
Santos-Hernandez29 <sh6074843@gmail.com>
2024-08-22 14:21:55 -0600

    Programa de calculo de edad
    Este programa calcula la edad en Meses, Dias y Horas

--- calculo/Program.cs
+++ calculo/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+class CALCULO
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su Nombre");
+        string Nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese su Edad");
+        int Edad = Convert.ToInt32(Console.ReadLine());
+        Double EdadMeses = Edad * 12;
+        Double EdadDias = Edad * 365;
+        Double EdadHoras = Edad * 365 * 24;
+        Console.WriteLine("la edad en meses es:" + EdadMeses + " Meses");
+        Console.WriteLine("La Edad en dias es:" + EdadDias + " Dias");
+        Console.WriteLine("La Edad en Horas es:" + EdadHoras + " Horas");
+    }
+}
+   
+
+
+ 
